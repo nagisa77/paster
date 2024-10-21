c716947bb3f2bca3229815a3f5fe714b2361456f
tim <nagisa12164@163.com>
2024-10-21 20:53:55 +0800

    feat: update

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/remote-locker-platform/' 
+    ? '/paster/' 
     : '/',
   transpileDependencies: true,
   configureWebpack: {
